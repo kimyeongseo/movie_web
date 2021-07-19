6c0f4206113e0f20b194a3ef1e0da82c0493340c
kimyeongseo <oseo0202@naver.com>
2021-07-19 19:52:36 +0900

    project set-up

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.render(<App />,document.getElementById("root"));
 
