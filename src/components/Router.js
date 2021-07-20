ade8be68d81bb2d31e5b28dd43bd10adfe2a0aab
kimyeongseo <oseo0202@naver.com>
2021-07-20 23:37:10 +0900

    what about Redirect and Switch

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 
 export default () => (
   <Router>
-    <>
+    <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" component={TV} />
       <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
       <Route path="/search" component={Search} />
-    </>
+      <Redirect from="*" to="/" />
+    </Switch>
   </Router>
 );
