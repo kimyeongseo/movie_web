fb088176337de0aa24c3fa5247a971b240dd6174
kimyeongseo <oseo0202@naver.com>
2021-07-20 23:20:48 +0900

    About React Router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 
+// HashRouter을 사용하는 이유: 간단함, 페이지의 Hash를 사용한다. 앱에 주로 사용
+// BrowserRouter은 HTML history API를 사용한다. 웹에 주로 사용
+
 export default () => (
   <Router>
     <>
-      <Route ath="/" exact component={Home} />
-      <Route ath="/tv" exact component={TV} />
-      <Route ath="/search" exact component={Search} />
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" component={TV} />
+      <Route path="/search" component={Search} />
     </>
   </Router>
 );
