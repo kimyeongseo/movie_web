c817c1d1abcc2f0f221e5718ee6918a0829e5e18
kimyeongseo <oseo0202@naver.com>
2021-07-23 21:17:58 +0900

    Detail Contanier

--- src/components/Router.js
+++ src/components/Router.js
@@ -8,6 +8,7 @@ import {
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
+import Detail from "Routes/Detail";
 import Header from "./Header";
 
 export default () => (
@@ -18,6 +19,8 @@ export default () => (
         <Route path="/" exact component={Home} />
         <Route path="/tv" exact component={TV} />
         <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
