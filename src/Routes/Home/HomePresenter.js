90c800472a47ee358a318f998ffe806c49f8d928
kimyeongseo <oseo0202@naver.com>
2021-07-24 20:43:00 +0900

    HomePresenter and Section Component

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,8 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const HomePresenter = ({ nowPlaying, popular, upcomfing, loading, error }) =>
-  null;
+import Section from "components/Section";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map((movie) => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movie">
+          {upcoming.map((movie) => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Movie">
+          {popular.map((movie) => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
