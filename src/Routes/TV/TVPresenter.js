13d567283d6ac79f7dd28b4727de870c540bb790
kimyeongseo <oseo0202@naver.com>
2021-07-24 21:03:31 +0900

    About grid

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -3,27 +3,36 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import Section from "components/Section";
+import Loader from "components/Loader";
 
 const Container = styled.div`
   padding: 0px 10px;
 `;
 
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
-  loading ? null : (
+  loading ? (
+    <Loader />
+  ) : (
     <Container>
       {topRated && topRated.length > 0 && (
         <Section title="Top Rated Shows">
-          {topRated.map((show) => show.name)}
+          {topRated.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
         </Section>
       )}
       {airingToday && airingToday.length > 0 && (
         <Section title="Airing Today ">
-          {airingToday.map((show) => show.name)}
+          {airingToday.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Show">
-          {popular.map((show) => show.name)}
+          {popular.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
         </Section>
       )}
     </Container>
